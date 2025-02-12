6876a58958d2d0e295953d527bde80e13832ea73
pedroaveber <pedro.veber@outlook.com>
2025-02-12 09:03:35 -0300

    Feat: Create Meal CRUD

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask, request, jsonify
 from database import db
 from models.user import User
@@ -71,6 +72,109 @@ def sign_in():
 def sign_out():
     logout_user()
     return jsonify({'message': 'User logged out'}), 200
+
+@app.route('/meals', methods=['POST'])
+@login_required
+def create_meal():
+    body = request.json
+
+    name = body.get('name')
+    healthy = body.get('healthy', False)
+    description = body.get('description')
+
+    if name and description:
+        meal = Meal(
+            name=name,
+            healthy=healthy,
+            description=description,
+            user_id=current_user.id,
+            created_at=datetime.now()
+        )
+
+        db.session.add(meal)
+        db.session.commit()
+
+        return jsonify({ 'message': 'Meal created successfully' }), 201
+
+@app.route('/meals', methods=['GET'])
+@login_required
+def get_meals():
+    meals = Meal.query.filter_by(user_id=current_user.id).all()
+
+    result = []
+
+    for meal in meals:
+        result.append({
+            'id': meal.id,
+            'name': meal.name,
+            'healthy': meal.healthy,
+            'created_at': meal.created_at,
+            'description': meal.description,
+        })
+
+    return jsonify(result), 200
+
+
+@app.route('/meals/<int:id>', methods=['GET'])
+@login_required
+def get_meal(id):
+    meal = Meal.query.filter_by(id=id, user_id=current_user.id).first()
+
+    if meal and meal.user_id != current_user.id:
+        return jsonify({ 'message': 'Unauthorized' }), 403
+
+    if meal:
+        return jsonify({
+            'id': meal.id,
+            'name': meal.name,
+            'healthy': meal.healthy,
+            'created_at': meal.created_at,
+            'description': meal.description,
+        }), 200
+
+    return jsonify({ 'message': 'Meal not found' }), 404
+
+@app.route('/meals/<int:id>', methods=['PUT'])
+@login_required
+def update_meal(id):
+    meal = Meal.query.filter_by(id=id, user_id=current_user.id).first()
+
+    if meal and meal.user_id != current_user.id:
+        return jsonify({ 'message': 'Unauthorized' }), 403
+
+    if meal:
+        body = request.json
+
+        name = body.get('name')
+        healthy = body.get('healthy', False)
+        description = body.get('description')
+
+        if name and description:
+            meal.name = name
+            meal.healthy = healthy
+            meal.description = description
+
+            db.session.commit()
+
+            return jsonify({ 'message': 'Meal updated successfully' }), 200
+
+    return jsonify({ 'message': 'Meal not found' }), 404
+
+@app.route('/meals/<int:id>', methods=['DELETE'])
+@login_required
+def delete_meal(id):
+    meal = Meal.query.filter_by(id=id, user_id=current_user.id).first()
+
+    if meal and meal.user_id != current_user.id:
+        return jsonify({ 'message': 'Unauthorized' }), 403
+
+    if meal:
+        db.session.delete(meal)
+        db.session.commit()
+
+        return jsonify({ 'message': 'Meal deleted successfully' }), 200
+
+    return jsonify({ 'message': 'Meal not found' }), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
